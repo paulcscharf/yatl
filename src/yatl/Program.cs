b1d9f7c380306722cbbcfa6f0f99846a0c9a13c2
amulware <amul@amulware.net>
2014-05-15 11:34:05 +0200

    set working directory to location of executable at start, and set thread culture to invariant

--- src/yatl/Program.cs
+++ src/yatl/Program.cs
@@ -1,10 +1,18 @@
-﻿
+
+using System.Globalization;
+using System.Threading;
+
 namespace yatl
 {
     static class Program
     {
         static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            System.Environment.CurrentDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+
             int glMajor = OpenTK.Configuration.RunningOnLinux ? 1 : 3;
             int glMinor = OpenTK.Configuration.RunningOnLinux ? 0 : 2;
             new YATLWindow(glMajor, glMinor).Run(Settings.General.DefaultFPS);
