23c6b2365edb5aeb85bce31939a0b99695a16fdf
Chiel92 <ctje92@gmail.com>
2014-05-15 11:36:36 +0200

    nop

--- src/yatl/Program.cs
+++ src/yatl/Program.cs
@@ -1,12 +1,14 @@
-﻿
+﻿using System;
+
 namespace yatl
 {
     static class Program
     {
         static void Main(string[] args)
         {
-            int glMajor = OpenTK.Configuration.RunningOnLinux ? 1 : 3;
-            int glMinor = OpenTK.Configuration.RunningOnLinux ? 0 : 2;
+            Console.WriteLine(OpenTK.Configuration.RunningOnLinux.ToString()); // Returns False!!
+            int glMajor = 1;//OpenTK.Configuration.RunningOnLinux ? 1 : 3;
+            int glMinor = 0;//OpenTK.Configuration.RunningOnLinux ? 0 : 2;
             new YATLWindow(glMajor, glMinor).Run(Settings.General.DefaultFPS);
         }
     }
