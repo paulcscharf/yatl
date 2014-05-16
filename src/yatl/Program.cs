b9392fb6e23bb96abd8541ef7f31f115f1b0e7e0
amulware <amul@amulware.net>
2014-05-16 22:48:00 +0200

    removed debug output from start of program

--- src/yatl/Program.cs
+++ src/yatl/Program.cs
@@ -9,28 +9,12 @@ namespace yatl
     {
         static void Main(string[] args)
         {
-            // Everything returns false on linux ??
-            foreach(bool config in new bool[]{
-                    TK.RunningOnAndroid,
-                    TK.RunningOnLinux,
-                    TK.RunningOnMacOS,
-                    TK.RunningOnMono,
-                    TK.RunningOnSdl2,
-                    TK.RunningOnUnix,
-                    TK.RunningOnWindows,
-                    TK.RunningOnX11,
-                    }){
-                Console.WriteLine(config.ToString());
-            }
-
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
             System.Environment.CurrentDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
             int glMajor = 1;
             int glMinor = 0;
-            //int glMajor = OpenTK.Configuration.RunningOnLinux ? 1 : 3;
-            //int glMinor = OpenTK.Configuration.RunningOnLinux ? 0 : 2;
             new YATLWindow(glMajor, glMinor).Run(Settings.General.DefaultFPS);
         }
     }
