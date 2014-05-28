28909fa0e86e871741361c7a52dfd064f8331223
Chiel92 <ctje92@gmail.com>
2014-05-28 21:06:39 +0200

    new files added -_-

---
+++ src/yatl/Music/Audible.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace yatl
+{
+    abstract class Audible
+    {
+        public abstract double Duration { get; }
+
+        public abstract IEnumerable<SoundEvent> Render();
+    }
+
+    /// <summary>
+    /// Atomic MusicObject
+    /// </summary>
+    class Note : Audible
+    {
+        Pitch pitch;
+        double duration;
+
+        public override double Duration { get { return this.duration; } }
+        public double Frequency { get { return this.pitch.Frequency; } }
+
+        public Note(int duration, Pitch pitch)
+        {
+            this.duration = duration;
+            this.pitch = pitch;
+        }
+
+        public override IEnumerable<SoundEvent> Render()
+        {
+            var start = new NoteOn(0, this);
+            yield return start;
+            var end = new NoteOff(this.Duration, start);
+            yield return end;
+        }
+
+        public override string ToString()
+        {
+            return this.Duration.ToString() + " " + this.pitch.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Set of MusicObjects that sound subsequently
+    /// </summary>
+    class Serial : Audible
+    {
+        Audible[] content;
+        public override double Duration { get { return this.content.Sum(o => o.Duration); } }
+
+        public Serial(Audible[] content)
+        {
+            this.content = content;
+        }
+
+        public override IEnumerable<SoundEvent> Render()
+        {
+            double time = 0;
+
+            foreach (var child in this.content) {
+                foreach (var soundEvent in child.Render()) {
+                    soundEvent.AddOffset(time);
+                    yield return soundEvent;
+                }
+                time += child.Duration;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", this.content.Select(obj => obj.ToString()));
+        }
+    }
+
+    /// <summary>
+    /// Set of MusicObjects that sound simultaneously
+    /// </summary>
+    class Parallel : Audible
+    {
+        Audible[] content;
+        double durationMultiplier;
+        double innerDuration;
+
+        public override double Duration { get { return this.innerDuration * this.durationMultiplier; } }
+
+        public Parallel(Audible[] content, int durationMultiplier = 1)
+        {
+            if (content.Length == 0)
+                throw new Exception("No empty content allowed.");
+
+            double duration = content[0].Duration;
+            Console.WriteLine(content[0].ToString());
+            Console.WriteLine(duration.ToString());
+            bool allSameDuration = content.All(o => o.Duration == duration);
+            if (!allSameDuration)
+                throw new Exception("Not every musicobject has the same duration.");
+
+            this.durationMultiplier = durationMultiplier;
+            this.innerDuration = duration;
+            this.content = content;
+        }
+
+        public override IEnumerable<SoundEvent> Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return this.durationMultiplier.ToString() + "{" + string.Join(",", this.content.Select(obj => obj.ToString())) + "}";
+        }
+    }
+}
---
+++ src/yatl/Music/SoundEvent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cireon.Audio;
+
+namespace yatl
+{
+    abstract class SoundEvent
+    {
+        public double StartTime;
+
+        public SoundEvent(double startTime)
+        {
+            this.StartTime = startTime;
+        }
+
+        public void AddOffset(double offsetTime)
+        {
+            this.StartTime += offsetTime;
+        }
+
+        public abstract void Execute(MusicManager manager);
+    }
+
+    class NoteOn : SoundEvent
+    {
+        public double StartTime;
+        public Note Note;
+        public Source Source = null;
+
+        public NoteOn(double startTime, Note note) : base(startTime)
+        {
+            this.Note = note;
+        }
+
+        public override void Execute(MusicManager manager)
+        {
+            this.Source = manager.PianoSound.GenerateSource();
+            this.Source.Pitch = (float) (this.Note.Frequency / 130.8);
+            this.Source.Play();
+            Console.WriteLine("Playing note " + this.ToString());
+        }
+    }
+
+    class NoteOff : SoundEvent
+    {
+        public double StartTime;
+        NoteOn noteOn;
+
+        public NoteOff(double startTime, NoteOn noteOn) : base(startTime)
+        {
+            this.noteOn = noteOn;
+        }
+
+        public override void Execute(MusicManager manager)
+        {
+            if (this.noteOn.Source == null)
+                throw new Exception("NoteOn must be executed before NoteOff.");
+            this.noteOn.Source.Stop();
+        }
+    }
+}
