96e167ed324918b49125f9eb7e0948d12d600bf8
Chiel92 <ctje92@gmail.com>
2014-05-28 12:54:55 +0200

    minor fixes

--- src/yatl/Music/MusicManager.cs
+++ src/yatl/Music/MusicManager.cs
@@ -12,11 +12,12 @@ namespace yatl
              BranchingMusicalComposition.FromFile(filename);
              AudioManager.Initialize();
              Console.WriteLine(AudioManager.Instance.MasterVolume);
-             AudioManager.Instance.MasterVolume = 2;
+             //AudioManager.Instance.MasterVolume = 2;
              Console.WriteLine(AudioManager.Instance.MasterVolume);
 
              var sf = new SoundFile("data/music/Piano.ogg");
              var source = sf.GenerateSource();
+             //source.Volume = 2;
              source.Play();
              System.Threading.Thread.Sleep(1000);
              source.Pitch = 2;
